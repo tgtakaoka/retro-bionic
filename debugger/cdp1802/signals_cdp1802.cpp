f571ee7e751e0f0d4eaefd25e3a52a8679f57166
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-01-08 17:24:48 +0900

    [CDP1802] Unify debugger

---
+++ debugger/cdp1802/signals_cdp1802.cpp
@@ -0,0 +1,54 @@
+#include "signals_cdp1802.h"
+#include "char_buffer.h"
+#include "debugger.h"
+#include "digital_bus.h"
+#include "pins_cdp1802.h"
+
+namespace debugger {
+namespace cdp1802 {
+
+void Signals::getStatus() {
+    auto s = digitalReadFast(PIN_SC0);
+    if (digitalReadFast(PIN_SC1))
+        s += 2;
+    sc() = s;
+}
+
+void Signals::getAddr1() {
+    addr = static_cast<uint16_t>(busRead(MA)) << 8;
+}
+
+void Signals::getAddr2() {
+    addr |= busRead(MA);
+}
+
+void Signals::getDirection() {
+    mrd() = digitalReadFast(PIN_MRD);
+    mwr() = digitalReadFast(PIN_MWR);
+}
+
+void Signals::getData() {
+    data = busRead(DBUS);
+}
+
+void Signals::print() const {
+    //                              01234567890123
+    static constexpr char line[] = "LW A=xxxx D=xx";
+    static auto &buffer = *new CharBuffer(line);
+    static constexpr char SC[] = {'F', ' ', 'D', 'I'};
+    buffer[0] = SC[sc()];
+    buffer[1] = read() ? 'R' : (write() ? 'W' : '-');
+    buffer.hex16(5, addr);
+    buffer.hex8(12, data);
+    cli.println(buffer);
+}
+
+}  // namespace cdp1802
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
