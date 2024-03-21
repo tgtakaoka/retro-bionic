tgtakaoka/RetroCyborg

[MC146805E] Unify debugger

debugger/mc6805/pins_mc146805e2.cpp
@@ -1,0 +1,294 @@
+#include "pins_mc146805e2.h"
+#include "debugger.h"
+#include "devs_mc146805e2.h"
+#include "digital_bus.h"
+#include "inst_mc146805.h"
+#include "mems_mc146805e2.h"
+#include "regs_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+using mc146805::Inst;
+
+struct PinsMc146805E2 Pins {
+    &Regs, &Inst, &Memory, &Devices
+};
+
+/**
+ * MC146805E bus cycle.
+ *      _    __    __    __    __    __    __    __    __    __    __
+ * OSC1  |_c|1 |_c|2 |_c|3 |_c|4 |_c|5 |_c|1 |_c|2 |_c|3 |_c|4 |_c|5 |__
+ *       \     \ ____\  \  \           \     \ ____\  \  \           \
+ *   AS __|_____|     |__|__|___________|_____|     |__|__|___________|_
+ *      __            \  |  |___________|           \  |  |___________|
+ *   DS   |____________|_|__|           |____________|_|__|           |_
+ *        \            |_|______________\____________| |
+ *   LI ___|___________| |               |           |_|________________
+ *      ___|_____________|_______________|             |
+ *  R/W ___|             |               |_____________|________________
+ *      ___________ _____|________________________ ____|________________
+ * ADRH ___________X_____|________________________X____|________________
+ *                   ____|            _____        ____|_______________
+ * DATA ------------<_AL_>-----------<__DR_>------<_AL_x________DW_____>-
+ */
+
+namespace {
+
+//  fOSC: min 5.0 MHz
+// tOLOL: min:  200 ns: osc_cycle
+//   tOH: min    75 ns: osc_hi
+//   tOL: min    75 ns: osc_lo
+//  tCYC: min 1,000 ns; ds_cycle
+// tPWEL: min   560 ns: ds_lo
+// tPWEH: min   375 ns: ds_hi
+//  tASD: min   160 ns: ds_lo to as_hi
+// PWASH: min   175 ns: as_hi
+// tASED: min   160 ns: as_lo to ds_hi
+//   tAD: max   300 ns: ds_lo to r/w
+//  tADH: max   100 ns: as_hi to addr
+//  tAHL: min    60 ns: as_lo to addr hold
+//  tDSR: min   115 ns: ds_hi to data
+//  tDHR: min   160 ns: ds_lo to data hold
+//  tDDW: max   120 ns: ds_hi to data
+//  tDHW: min    55 ns: ds_lo to data hold
+//   tRL: min 1,500 ns; reset_lo
+constexpr auto osc1_ds_ns = 100;
+constexpr auto osc1_hi_ns = 84;     // 100
+constexpr auto osc1_lo_ns = 76;     // 100
+constexpr auto c1_lo_ns = 68;       // 100
+constexpr auto c1_hi_ns = 56;       // 100
+constexpr auto c2_lo_ns = 38;       // 100
+constexpr auto c2_hi_ns = 78;       // 100
+constexpr auto c3_lo_ns = 48;       // 100
+constexpr auto c3_hi_ns = 48;       // 100
+constexpr auto c4_lo_read = 0;      // 100
+constexpr auto c4_lo_inject = 0;    // 100
+constexpr auto c4_hi_read = 28;     // 100
+constexpr auto c5_lo_read = 80;     // 100
+constexpr auto c4_lo_write = 4;     // 100
+constexpr auto c4_hi_write = 48;    // 100
+constexpr auto c5_lo_write = 0;     // 100
+constexpr auto c5_lo_capture = 68;  // 100
+constexpr auto c5_hi_ns = 32;       // 100
+constexpr auto tpcs_ns = 200;
+
+inline void osc1_hi() __attribute__((always_inline));
+inline void osc1_hi() {
+    digitalWriteFast(PIN_OSC1, HIGH);
+}
+
+inline void osc1_lo() __attribute__((always_inline));
+inline void osc1_lo() {
+    digitalWriteFast(PIN_OSC1, LOW);
+}
+
+inline void clock_cycle() __attribute__((always_inline));
+inline void clock_cycle() {
+    delayNanoseconds(osc1_lo_ns);
+    osc1_hi();
+    delayNanoseconds(osc1_hi_ns);
+    osc1_lo();
+}
+
+uint8_t signal_ds() {
+    return digitalReadFast(PIN_DS);
+}
+
+void assert_irq() {
+    digitalWriteFast(PIN_IRQ, LOW);
+}
+
+void negate_irq() {
+    digitalWriteFast(PIN_IRQ, HIGH);
+}
+
+void assert_reset() {
+    // Drive RESET condition
+    digitalWriteFast(PIN_RESET, LOW);
+    negate_irq();
+}
+
+void negate_reset() {
+    digitalWriteFast(PIN_RESET, HIGH);
+}
+
+const uint8_t PINS_LOW[] = {
+        PIN_OSC1,
+        PIN_RESET,
+        PIN_TIMER,
+};
+
+const uint8_t PINS_HIGH[] = {
+        PIN_IRQ,
+};
+
+const uint8_t PINS_INPUT[] = {
+        PIN_B0,
+        PIN_B1,
+        PIN_B2,
+        PIN_B3,
+        PIN_B4,
+        PIN_B5,
+        PIN_B6,
+        PIN_B7,
+        PIN_AH8,
+        PIN_AH9,
+        PIN_AH10,
+        PIN_AH11,
+        PIN_AH12,
+        PIN_AS,
+        PIN_DS,
+        PIN_RW,
+        PIN_LI,
+};
+
+}  // namespace
+
+void PinsMc146805E2::reset() {
+    pinsMode(PINS_INPUT, sizeof(PINS_INPUT), INPUT_PULLDOWN);
+    pinsMode(PINS_LOW, sizeof(PINS_LOW), OUTPUT, LOW);
+    pinsMode(PINS_HIGH, sizeof(PINS_HIGH), OUTPUT, HIGH);
+    pinsMode(PINS_INPUT, sizeof(PINS_INPUT), INPUT_PULLDOWN);
+
+    // #RESET must stay low for a minimum of one tRL (1.5usec at VDD=5V).
+    assert_reset();
+    for (auto i = 0; i < 15 * 5; i++)
+        clock_cycle();
+
+    // Synchronize clock output to DS.
+    while (signal_ds() == LOW) {
+        clock_cycle();
+        delayNanoseconds(osc1_ds_ns);
+    }
+    while (signal_ds() != LOW) {
+        clock_cycle();
+        delayNanoseconds(osc1_ds_ns);
+    }
+    // DS=L
+
+    const auto vec_reset = _mems->vecReset();
+    const auto vector = _mems->raw_read16(vec_reset) & _mems->maxAddr();
+    // If reset vector pointing internal memory, we can't inject instructions.
+    _mems->raw_write16(vec_reset, 0x1000);
+
+    Signals::resetCycles();
+    cycle();
+    cycle();
+    delayNanoseconds(tpcs_ns);
+    negate_reset();
+    prepareCycle();
+    suspend();
+
+    // We should certainly inject SWI by pointing external address here.
+    _regs->save();
+    // Restore reset vector
+    _mems->raw_write16(vec_reset, vector);
+    _regs->setIp(vector);
+}
+
+mc6805::Signals *PinsMc146805E2::currCycle() const {
+    auto s = Signals::put();
+    s->getDirection();
+    s->getMuxedAddr();
+    s->getHighAddr();
+    s->getLoadInstruction();
+    return s;
+}
+
+mc6805::Signals *PinsMc146805E2::rawPrepareCycle() const {
+    // MC146805E bus cycle is CLK/5, so we toggle CLK 5 times c1
+    // c1
+    osc1_hi();
+    // To ensure 160ns data hold time after DS-falling edge.
+    busMode(B, INPUT);
+    delayNanoseconds(c1_hi_ns);
+    osc1_lo();  // AS->LOW
+    // c2
+    delayNanoseconds(c2_lo_ns);
+    auto s = Signals::put();
+    s->getDirection();
+    osc1_hi();
+    delayNanoseconds(c2_hi_ns);
+    // c3
+    osc1_lo();  // AS->LOW
+    delayNanoseconds(c3_lo_ns);
+    s->getMuxedAddr();
+    osc1_hi();
+    s->getHighAddr();
+    delayNanoseconds(c3_hi_ns);
+    // c4
+    osc1_lo();
+    s->getLoadInstruction();
+    // DS=HIGH
+
+    return s;
+}
+
+mc6805::Signals *PinsMc146805E2::prepareCycle() const {
+    delayNanoseconds(c1_lo_ns);
+    return rawPrepareCycle();
+}
+
+mc6805::Signals *PinsMc146805E2::completeCycle(mc6805::Signals *signals) const {
+    auto s = static_cast<Signals *>(signals);
+    if (s->write()) {
+        delayNanoseconds(c4_lo_write);
+        // c4
+        osc1_hi();
+        s->getData();
+        delayNanoseconds(c4_hi_write);
+        // c5
+        osc1_lo();  // DS=HIGH
+        if (s->writeMemory()) {
+            _mems->write(s->addr, s->data);
+            if (c5_lo_write)
+                delayNanoseconds(c5_lo_write);
+        } else {
+            delayNanoseconds(c5_lo_capture);
+        }
+    } else {
+        if (s->readMemory()) {
+            s->data = _mems->read(s->addr);
+            if (c4_lo_read)
+                delayNanoseconds(c4_lo_read);
+        } else {
+            delayNanoseconds(c4_lo_inject);
+        }
+        // c4
+        osc1_hi();
+        busWrite(B, s->data);
+        busMode(B, OUTPUT);
+        delayNanoseconds(c4_hi_read);
+        // c5
+        osc1_lo();  // DS=HIGH
+        delayNanoseconds(c5_lo_read);
+    }
+    osc1_hi();
+    Signals::nextCycle();
+    delayNanoseconds(c5_hi_ns);
+    // Data hold time will be done in next cycle().
+    osc1_lo();  // DS->LOW
+
+    return s;
+}
+
+void PinsMc146805E2::assertInt(uint8_t name) {
+    (void)name;
+    assert_irq();
+}
+
+void PinsMc146805E2::negateInt(uint8_t name) {
+    (void)name;
+    negate_irq();
+}
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:

debugger/mc6805/pins_mc146805e2.h
@@ -1,0 +1,75 @@
+#ifndef __PINS_MC146805E2_H__
+#define __PINS_MC146805E2_H__
+
+#define ACIA_BASE 0x17F8
+
+#define PORT_B 6     /* GPIO6 */
+#define B_gp 16      /* P6.16-P6.23 */
+#define B_gm 0xFF    /* P6.00-P6.07 */
+#define B_vp 0       /* B0-B7 */
+#define PIN_B0 19    /* P6.16 */
+#define PIN_B1 18    /* P6.17 */
+#define PIN_B2 14    /* P6.18 */
+#define PIN_B3 15    /* P6.19 */
+#define PIN_B4 40    /* P6.20 */
+#define PIN_B5 41    /* P6.21 */
+#define PIN_B6 17    /* P6.22 */
+#define PIN_B7 16    /* P6.23 */
+#define PORT_AH 6    /* GPIO6 */
+#define AH_gp 24     /* P6.24-P6.28 */
+#define AH_gm 0x1F   /* P6.24-P6.28 */
+#define AH_vp 8      /* A8-A12 */
+#define PIN_AH8 22   /* P6.24 */
+#define PIN_AH9 23   /* P6.25 */
+#define PIN_AH10 20  /* P6.26 */
+#define PIN_AH11 21  /* P6.27 */
+#define PIN_AH12 38  /* P6.28 */
+#define PIN_AS 2     /* P9.04 */
+#define PIN_RW 3     /* P9.05 */
+#define PIN_IRQ 4    /* P9.06 */
+#define PIN_TIMER 1  /* P6.02 */
+#define PIN_OSC1 5   /* P9.08 */
+#define PIN_DS 6     /* P7.10 */
+#define PIN_LI 9     /* P7.11 */
+#define PIN_RESET 28 /* P8.18 */
+
+#include "pins_mc6805.h"
+#include "signals_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+using mc6805::InstMc6805;
+using mc6805::MemsMc6805;
+using mc6805::PinsMc6805;
+using mc6805::RegsMc6805;
+
+struct PinsMc146805E2 final : PinsMc6805 {
+    PinsMc146805E2(RegsMc6805 *regs, const InstMc6805 *inst,
+            const MemsMc6805 *mems, Devs *devs)
+        : PinsMc6805(regs, inst, mems, devs) {}
+
+    void reset() override;
+    void assertInt(uint8_t name) override;
+    void negateInt(uint8_t name) override;
+
+protected:
+    mc6805::Signals *currCycle() const override;
+    mc6805::Signals *rawPrepareCycle() const override;
+    mc6805::Signals *prepareCycle() const override;
+    mc6805::Signals *completeCycle(mc6805::Signals *signals) const override;
+};
+
+extern struct PinsMc146805E2 Pins;
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+#endif
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:

debugger/mc6805/signals_mc146805e2.cpp
@@ -1,0 +1,37 @@
+#include "signals_mc146805e2.h"
+#include "debugger.h"
+#include "digital_bus.h"
+#include "pins_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+void Signals::getDirection() {
+    rw() = digitalReadFast(PIN_RW);
+}
+
+void Signals::getMuxedAddr() {
+    addr = busRead(B);
+}
+
+void Signals::getHighAddr() {
+    addr |= busRead(AH);
+}
+
+void Signals::getLoadInstruction() {
+    li() = digitalReadFast(PIN_LI);
+}
+
+void Signals::getData() {
+    data = busRead(B);
+}
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:

tgtakaoka/retro-bionic

debugger/mc6805/pins_mc146805e2.cpp
@@ -1,0 +1,292 @@
+#include "pins_mc146805e2.h"
+#include "debugger.h"
+#include "devs_mc146805e2.h"
+#include "digital_bus.h"
+#include "inst_mc146805.h"
+#include "mems_mc146805e2.h"
+#include "regs_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+using mc146805::Inst;
+
+struct PinsMc146805E2 Pins {
+    &Regs, &Inst, &Memory, &Devices
+};
+
+/**
+ * MC146805E bus cycle.
+ *      _    __    __    __    __    __    __    __    __    __    __
+ * OSC1  |_c|1 |_c|2 |_c|3 |_c|4 |_c|5 |_c|1 |_c|2 |_c|3 |_c|4 |_c|5 |__
+ *       \     \ ____\  \  \           \     \ ____\  \  \           \
+ *   AS __|_____|     |__|__|___________|_____|     |__|__|___________|_
+ *      __            \  |  |___________|           \  |  |___________|
+ *   DS   |____________|_|__|           |____________|_|__|           |_
+ *        \            |_|______________\____________| |
+ *   LI ___|___________| |               |           |_|________________
+ *      ___|_____________|_______________|             |
+ *  R/W ___|             |               |_____________|________________
+ *      ___________ _____|________________________ ____|________________
+ * ADRH ___________X_____|________________________X____|________________
+ *                   ____|            _____        ____|_______________
+ * DATA ------------<_AL_>-----------<__DR_>------<_AL_x________DW_____>-
+ */
+
+namespace {
+
+//  fOSC: min 5.0 MHz
+// tOLOL: min:  200 ns: osc_cycle
+//   tOH: min    75 ns: osc_hi
+//   tOL: min    75 ns: osc_lo
+//  tCYC: min 1,000 ns; ds_cycle
+// tPWEL: min   560 ns: ds_lo
+// tPWEH: min   375 ns: ds_hi
+//  tASD: min   160 ns: ds_lo to as_hi
+// PWASH: min   175 ns: as_hi
+// tASED: min   160 ns: as_lo to ds_hi
+//   tAD: max   300 ns: ds_lo to r/w
+//  tADH: max   100 ns: as_hi to addr
+//  tAHL: min    60 ns: as_lo to addr hold
+//  tDSR: min   115 ns: ds_hi to data
+//  tDHR: min   160 ns: ds_lo to data hold
+//  tDDW: max   120 ns: ds_hi to data
+//  tDHW: min    55 ns: ds_lo to data hold
+//   tRL: min 1,500 ns; reset_lo
+constexpr auto osc1_ds_ns = 100;
+constexpr auto osc1_hi_ns = 84;     // 100
+constexpr auto osc1_lo_ns = 76;     // 100
+constexpr auto c1_lo_ns = 68;       // 100
+constexpr auto c1_hi_ns = 56;       // 100
+constexpr auto c2_lo_ns = 38;       // 100
+constexpr auto c2_hi_ns = 78;       // 100
+constexpr auto c3_lo_ns = 48;       // 100
+constexpr auto c3_hi_ns = 48;       // 100
+constexpr auto c4_lo_read = 0;      // 100
+constexpr auto c4_lo_inject = 0;    // 100
+constexpr auto c4_hi_read = 28;     // 100
+constexpr auto c5_lo_read = 80;     // 100
+constexpr auto c4_lo_write = 4;     // 100
+constexpr auto c4_hi_write = 48;    // 100
+constexpr auto c5_lo_write = 0;     // 100
+constexpr auto c5_lo_capture = 68;  // 100
+constexpr auto c5_hi_ns = 32;       // 100
+constexpr auto tpcs_ns = 200;
+
+inline void osc1_hi() __attribute__((always_inline));
+inline void osc1_hi() {
+    digitalWriteFast(PIN_OSC1, HIGH);
+}
+
+inline void osc1_lo() __attribute__((always_inline));
+inline void osc1_lo() {
+    digitalWriteFast(PIN_OSC1, LOW);
+}
+
+inline void clock_cycle() __attribute__((always_inline));
+inline void clock_cycle() {
+    delayNanoseconds(osc1_lo_ns);
+    osc1_hi();
+    delayNanoseconds(osc1_hi_ns);
+    osc1_lo();
+}
+
+uint8_t signal_ds() {
+    return digitalReadFast(PIN_DS);
+}
+
+void assert_irq() {
+    digitalWriteFast(PIN_IRQ, LOW);
+}
+
+void negate_irq() {
+    digitalWriteFast(PIN_IRQ, HIGH);
+}
+
+void assert_reset() {
+    // Drive RESET condition
+    digitalWriteFast(PIN_RESET, LOW);
+    negate_irq();
+}
+
+void negate_reset() {
+    digitalWriteFast(PIN_RESET, HIGH);
+}
+
+const uint8_t PINS_LOW[] = {
+        PIN_OSC1,
+        PIN_RESET,
+        PIN_TIMER,
+};
+
+const uint8_t PINS_HIGH[] = {
+        PIN_IRQ,
+};
+
+const uint8_t PINS_INPUT[] = {
+        PIN_B0,
+        PIN_B1,
+        PIN_B2,
+        PIN_B3,
+        PIN_B4,
+        PIN_B5,
+        PIN_B6,
+        PIN_B7,
+        PIN_ADDR8,
+        PIN_ADDR9,
+        PIN_ADDR10,
+        PIN_ADDR11,
+        PIN_ADDR12,
+        PIN_AS,
+        PIN_DS,
+        PIN_RW,
+        PIN_LI,
+};
+
+}  // namespace
+
+void PinsMc146805E2::reset() {
+    pinsMode(PINS_INPUT, sizeof(PINS_INPUT), INPUT_PULLDOWN);
+    pinsMode(PINS_LOW, sizeof(PINS_LOW), OUTPUT, LOW);
+    pinsMode(PINS_HIGH, sizeof(PINS_HIGH), OUTPUT, HIGH);
+    pinsMode(PINS_INPUT, sizeof(PINS_INPUT), INPUT_PULLDOWN);
+
+    // #RESET must stay low for a minimum of one tRL (1.5usec at VDD=5V).
+    assert_reset();
+    for (auto i = 0; i < 15 * 5; i++)
+        clock_cycle();
+
+    // Synchronize clock output to DS.
+    while (signal_ds() == LOW) {
+        clock_cycle();
+        delayNanoseconds(osc1_ds_ns);
+    }
+    while (signal_ds() != LOW) {
+        clock_cycle();
+        delayNanoseconds(osc1_ds_ns);
+    }
+    // DS=L
+
+    const auto vec_reset = _mems->vecReset();
+    const auto vector = _mems->raw_read16(vec_reset) & _mems->maxAddr();
+    // If reset vector pointing internal memory, we can't inject instructions.
+    _mems->raw_write16(vec_reset, 0x1000);
+
+    Signals::resetCycles();
+    cycle();
+    cycle();
+    delayNanoseconds(tpcs_ns);
+    negate_reset();
+    prepareCycle();
+    suspend();
+
+    // We should certainly inject SWI by pointing external address here.
+    _regs->save();
+    // Restore reset vector
+    _mems->raw_write16(vec_reset, vector);
+    _regs->setIp(vector);
+}
+
+mc6805::Signals *PinsMc146805E2::currCycle() const {
+    auto s = Signals::put();
+    s->getDirection();
+    s->getAddr();
+    s->getLoadInstruction();
+    return s;
+}
+
+mc6805::Signals *PinsMc146805E2::rawPrepareCycle() const {
+    // MC146805E bus cycle is CLK/5, so we toggle CLK 5 times c1
+    // c1
+    osc1_hi();
+    // To ensure 160ns data hold time after DS-falling edge.
+    busMode(B, INPUT);
+    delayNanoseconds(c1_hi_ns);
+    osc1_lo();  // AS->LOW
+    // c2
+    delayNanoseconds(c2_lo_ns);
+    auto s = Signals::put();
+    s->getDirection();
+    osc1_hi();
+    delayNanoseconds(c2_hi_ns);
+    // c3
+    osc1_lo();  // AS->LOW
+    delayNanoseconds(c3_lo_ns);
+    s->getAddr();
+    osc1_hi();
+    delayNanoseconds(c3_hi_ns);
+    s->getLoadInstruction();
+    // c4
+    osc1_lo();
+    // DS=HIGH
+
+    return s;
+}
+
+mc6805::Signals *PinsMc146805E2::prepareCycle() const {
+    delayNanoseconds(c1_lo_ns);
+    return rawPrepareCycle();
+}
+
+mc6805::Signals *PinsMc146805E2::completeCycle(mc6805::Signals *signals) const {
+    auto s = static_cast<Signals *>(signals);
+    if (s->write()) {
+        delayNanoseconds(c4_lo_write);
+        // c4
+        osc1_hi();
+        s->getData();
+        delayNanoseconds(c4_hi_write);
+        // c5
+        osc1_lo();  // DS=HIGH
+        if (s->writeMemory()) {
+            _mems->write(s->addr, s->data);
+            if (c5_lo_write)
+                delayNanoseconds(c5_lo_write);
+        } else {
+            delayNanoseconds(c5_lo_capture);
+        }
+    } else {
+        if (s->readMemory()) {
+            s->data = _mems->read(s->addr);
+            if (c4_lo_read)
+                delayNanoseconds(c4_lo_read);
+        } else {
+            delayNanoseconds(c4_lo_inject);
+        }
+        // c4
+        osc1_hi();
+        busMode(B, OUTPUT);
+        busWrite(B, s->data);
+        delayNanoseconds(c4_hi_read);
+        // c5
+        osc1_lo();  // DS=HIGH
+        delayNanoseconds(c5_lo_read);
+    }
+    osc1_hi();
+    Signals::nextCycle();
+    delayNanoseconds(c5_hi_ns);
+    // Data hold time will be done in next cycle().
+    osc1_lo();  // DS->LOW
+
+    return s;
+}
+
+void PinsMc146805E2::assertInt(uint8_t name) {
+    (void)name;
+    assert_irq();
+}
+
+void PinsMc146805E2::negateInt(uint8_t name) {
+    (void)name;
+    negate_irq();
+}
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:

debugger/mc6805/pins_mc146805e2.h
@@ -1,0 +1,75 @@
+#ifndef __PINS_MC146805E2_H__
+#define __PINS_MC146805E2_H__
+
+#define ACIA_BASE 0x17F8
+
+#define PORT_B 6       /* GPIO6 */
+#define B_gp 16        /* P6.16-P6.23 */
+#define B_gm 0xFF      /* P6.00-P6.07 */
+#define B_vp 0         /* B0-B7 */
+#define PIN_B0 19      /* P6.16 */
+#define PIN_B1 18      /* P6.17 */
+#define PIN_B2 14      /* P6.18 */
+#define PIN_B3 15      /* P6.19 */
+#define PIN_B4 40      /* P6.20 */
+#define PIN_B5 41      /* P6.21 */
+#define PIN_B6 17      /* P6.22 */
+#define PIN_B7 16      /* P6.23 */
+#define PORT_ADDR 6    /* GPIO6 */
+#define ADDR_gp 16     /* P6.16-P6.28 */
+#define ADDR_gm 0x1FFF /* P6.16-P6.28 */
+#define ADDR_vp 0      /* A8-A12 */
+#define PIN_ADDR8 22   /* P6.24 */
+#define PIN_ADDR9 23   /* P6.25 */
+#define PIN_ADDR10 20  /* P6.26 */
+#define PIN_ADDR11 21  /* P6.27 */
+#define PIN_ADDR12 38  /* P6.28 */
+#define PIN_AS 2       /* P9.04 */
+#define PIN_RW 3       /* P9.05 */
+#define PIN_IRQ 4      /* P9.06 */
+#define PIN_TIMER 1    /* P6.02 */
+#define PIN_OSC1 5     /* P9.08 */
+#define PIN_DS 6       /* P7.10 */
+#define PIN_LI 9       /* P7.11 */
+#define PIN_RESET 28   /* P8.18 */
+
+#include "pins_mc6805.h"
+#include "signals_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+using mc6805::InstMc6805;
+using mc6805::MemsMc6805;
+using mc6805::PinsMc6805;
+using mc6805::RegsMc6805;
+
+struct PinsMc146805E2 final : PinsMc6805 {
+    PinsMc146805E2(RegsMc6805 *regs, const InstMc6805 *inst,
+            const MemsMc6805 *mems, Devs *devs)
+        : PinsMc6805(regs, inst, mems, devs) {}
+
+    void reset() override;
+    void assertInt(uint8_t name) override;
+    void negateInt(uint8_t name) override;
+
+protected:
+    mc6805::Signals *currCycle() const override;
+    mc6805::Signals *rawPrepareCycle() const override;
+    mc6805::Signals *prepareCycle() const override;
+    mc6805::Signals *completeCycle(mc6805::Signals *signals) const override;
+};
+
+extern struct PinsMc146805E2 Pins;
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+#endif
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:

debugger/mc6805/signals_mc146805e2.cpp
@@ -1,0 +1,33 @@
+#include "signals_mc146805e2.h"
+#include "debugger.h"
+#include "digital_bus.h"
+#include "pins_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+void Signals::getDirection() {
+    rw() = digitalReadFast(PIN_RW);
+}
+
+void Signals::getAddr() {
+    addr = busRead(ADDR);
+}
+
+void Signals::getLoadInstruction() {
+    li() = digitalReadFast(PIN_LI);
+}
+
+void Signals::getData() {
+    data = busRead(B);
+}
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
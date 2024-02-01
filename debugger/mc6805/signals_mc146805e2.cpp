696c161add9c34c35c6e199b5aef4596a1aff5e9
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-02-01 22:50:45 +0900

    [MC146805E] Unify debugger

---
+++ debugger/mc6805/signals_mc146805e2.cpp
@@ -0,0 +1,33 @@
+#include "signals_mc146805e2.h"
+#include "debugger.h"
+#include "digital_bus.h"
+#include "pins_mc146805e2.h"
+
+namespace debugger {
+namespace mc146805e2 {
+
+void Signals::getDirection() {
+    rw() = digitalReadFast(PIN_RW);
+}
+
+void Signals::getAddr() {
+    addr = busRead(ADDR);
+}
+
+void Signals::getLoadInstruction() {
+    li() = digitalReadFast(PIN_LI);
+}
+
+void Signals::getData() {
+    data = busRead(B);
+}
+
+}  // namespace mc146805e2
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
---
+++ debugger/mc6805/signals_mc6805.cpp
@@ -0,0 +1,29 @@
+#include "signals_mc6805.h"
+#include "char_buffer.h"
+#include "debugger.h"
+#include "digital_bus.h"
+#include "pins_mc6805.h"
+
+namespace debugger {
+namespace mc6805 {
+
+void Signals::print() const {
+    LOG(cli.printDec(pos(), -4));
+    //                              0123456789012
+    static constexpr char line[] = "W A=xxxx D=xx";
+    static auto &buffer = *new CharBuffer(line);
+    buffer[0] = fetch() ? 'L' : (write() ? 'W' : 'R');
+    buffer.hex16(4, addr);
+    buffer.hex8(11, data);
+    cli.println(buffer);
+}
+
+}  // namespace mc6805
+}  // namespace debugger
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// End:
+// vim: set ft=cpp et ts=4 sw=4:
