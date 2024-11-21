c3879e5bc3dba396aeac384ab7a57a6420d8f043
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-11-21 09:46:36 +0900

    [TMS7000] Fix TMS700x definitions

---
+++ samples/tms7000/cmosir.asm
@@ -0,0 +1,4 @@
+;;; -*- mode: asm; mode: flyspell-prog; -*-
+        cpu     tms7000
+        include "tms70c02.inc"
+        include "serialir.inc"
---
+++ samples/tms7000/nmosir.asm
@@ -0,0 +1,4 @@
+;;; -*- mode: asm; mode: flyspell-prog; -*-
+        cpu     tms7000
+        include "tms7002.inc"
+        include "serialir.inc"
