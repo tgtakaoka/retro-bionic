fd7c1c6d0a8667ed6f44dfdc5d6cb30fe80671b2
Tadashi G. Takaoka <tadashi.g.takaoka@gmail.com>
2024-12-08 14:23:34 +0900

    [base] Add power LED and Debug jumper

--- debugger/config_debugger.h
+++ debugger/config_debugger.h
@@ -8,12 +8,18 @@
 #define ENABLE_LOGGER
 #define Logger SerialUSB1
 
-#define PIN_USRSW 24  /* P6.12: Run LED */
-#define PIN_USRLED 25 /* P6.13: Halt Switch */
+#define PIN_USRSW 24  /* P6.12: Halt Switch */
 #define PIN_ID 34     /* P7.29: Identifity EEPROM UNI/O SCIO pin */
-#define PIN_DEBUG 35  /* P7.28: Debug */
 
-//#define WITH_ASSEMBLER
+#define ENABLE_USRLED
+#if defined(ENABLE_USRLED)
+#define PIN_USRLED 25 /* P6.13: User LED */
+#define PIN_DEBUG 35  /* P7.28: Debug */
+#else
+#define PIN_DEBUG 25  /* P6.13: Debug */
+#endif
+
+// #define WITH_ASSEMBLER
 #define WITH_DISASSEMBLER
 
 #endif
